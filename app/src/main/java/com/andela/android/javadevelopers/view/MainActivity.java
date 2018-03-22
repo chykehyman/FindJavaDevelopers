62d75f736c6b02680a8bddfa6fdc1b3d8ccc24d2
Chike <chike@Chike.local>
2018-03-22 11:50:48 +0100

    feat(ui/ux): improve UI/UX experience
    - implement grid view layout for developers list
    - implement progress dialog
    - implement swipe on refresh
    
    [Starts #156037248]

--- app/src/main/java/com/andela/android/javadevelopers/view/MainActivity.java
+++ app/src/main/java/com/andela/android/javadevelopers/view/MainActivity.java
@@ -1,9 +1,12 @@
 package com.andela.android.javadevelopers.view;
 
+import android.app.ProgressDialog;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.andela.android.javadevelopers.R;
 import com.andela.android.javadevelopers.adapter.ListAdapter;
@@ -16,22 +19,39 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements DeveloperPresenter.View {
     public ArrayList<DevelopersList> developersList;
     DeveloperPresenter developerPresenter = new DeveloperPresenter(this);
+
     public static final String DEVELOPERS_LIST = "saved_state";
+
     RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+    RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+
+    ProgressDialog progressDialog;
+    SwipeRefreshLayout swipeContainer;
 
 
     @Override
     public void displayDevelopersList(ArrayList<DevelopersList> listOfDevelopers) {
         developersList = listOfDevelopers;
+
         if (developersList != null) {
             recyclerView = findViewById(R.id.recyclerView);
             recyclerView.setHasFixedSize(true);
 
             recyclerView.setLayoutManager(layoutManager);
+            recyclerView.getLayoutManager().isAttachedToWindow();
 
             RecyclerView.Adapter adapter = new ListAdapter(developersList);
             recyclerView.setAdapter(adapter);
+        }
+
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+
+        if (swipeContainer != null && swipeContainer.isRefreshing()) {
+            swipeContainer.setRefreshing(false);
+            Toast.makeText(this, "Developers list refreshed",
+                    Toast.LENGTH_LONG).show();
         }
     }
 
@@ -44,8 +64,24 @@ public class MainActivity extends AppCompatActivity implements DeveloperPresente
             developersList = savedInstanceState.getParcelableArrayList(DEVELOPERS_LIST);
             displayDevelopersList(developersList);
         } else {
+            progressDialog = ProgressDialog.show(this, "Kenya Java Developers",
+                    "Loading... Please wait!!!", false, false);
+
             developerPresenter.getDevelopers();
         }
+
+        swipeContainer = findViewById(R.id.swipe_container);
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                developerPresenter.getDevelopers();
+            }
+        });
 
     }
 
